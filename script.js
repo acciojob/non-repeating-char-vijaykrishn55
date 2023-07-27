3cd47a2e31bc41e74052bd379b32f4e874d1afb9
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-27 14:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 //your JS code here. If required.
 function non(input) {
-	const n=input.length;
-	let char='';
-	let j=1;
-	for(let i=0;i<n;i++){
-		if(input.charAt(i)!=input.charAt(j) && j<input.length{
-			return input.charAt(i);
-			break;
-		}	
-		j++;
+	let n=input.length;
+	let map=new Map();
+	for (let i=0;i<n;i++){
+		if (map.get(input[i])){
+			map.set(input[i],map.get(input[i])+1)
+		}else{
+			map.set(input[i],1)
+		}
 	}
+	let ans='';
+	for (let i of input){
+		if (map.get(i)===1){
+			return i;
+		}
+	}
+	return ans;
 }
 function main() {
-	let input=prompt("Enter a string");
-	alert(non(input));
-	alert("hi there");
-}  
+	let input=prompt("Enter a string"); 
+   
+	let fun=non(input)
+	alert(fun);
+	
+}   
 main();

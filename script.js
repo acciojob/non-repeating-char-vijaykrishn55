30556964fde0681a3c362d7a4b0dd067803c8837
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-27 13:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function non(input) {
+	const n=input.length;
+	let char='';
+	let j=1;
+	for(let i=0;i<n;i++){
+		if(input.charAt(i)!=input.charAt(j) && j<input.length{
+			return input.charAt(i);
+			break;
+		}	
+		j++;
+	}
+}
+function main() {
+	let input=prompt("Enter a string");
+	alert(non(input));
+	alert("hi there");
+}  
+main();
